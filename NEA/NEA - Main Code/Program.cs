63096def992c682bae36880cb277e046a7bc5ae0
jagos <23JR3660@barton.ac.uk>
2024-08-15 20:48:34 +0100

    Implements stich subroutines and (not yet working) quick sort.

--- NEA/NEA - Main Code/Program.cs
+++ NEA/NEA - Main Code/Program.cs
@@ -1,40 +1,36 @@
 ﻿using System; //test
 using System.Collections.Generic; //test2
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NEA___Main_Code
 {
     internal class Program
     {
         static int SortKey = 1; //global key to organise the order of multiple tests completed in one instance
-        static int[] makeSet(bool evenlydis, bool ordered, int size, int range)
+        public struct arrayPair
+        {
+            public int[] left, right;
+        }
+        static int[] makeSet(bool evenlyDistributed, bool ordered, int size, int range)
         {
             int[] set = new int[size];
             Random rnd = new Random();
-            if (evenlydis && ordered)
+            if (evenlyDistributed && ordered)
             {
-                for (int i = 0; i < size; i++)
+                for (int i = 0; i < size; i++) 
                 {
-                    set[i] = i;
+                    set[i] = i+1; //i+1 is inserted so as to not include 0 in sets for the sake of readability
                 }
                 return set;
             }
-            if (evenlydis)
+            if (evenlyDistributed)
             {
                 for (int i = 0; i < size; ++i)
                 {
-                    set[i] = i;
+                    set[i] = i+1; 
                 }
-                return shuffle(set);
+                shuffle(set);
             }
             for (int i = 0; i < size; ++i)
             {
@@ -43,7 +39,14 @@ namespace NEA___Main_Code
             return set;
 
         }
-        static int[] shuffle(int[] set) //completes a random swap one time for every element that the array has
+        static void swap(int[] set, int indexOne, int indexTwo)
+        {
+            int temp;
+            temp = set[indexOne];
+            set[indexOne] = set[indexTwo];
+            set[indexTwo] = temp;
+        }
+        static void shuffle(int[] set) //completes a random swap one time for every element that the array has
         {
             Random rnd = new Random();
             int temp;
@@ -51,11 +54,8 @@ namespace NEA___Main_Code
             for (int i = 0; i < set.Length; i++)
             {
                 index = rnd.Next(0, set.Length);
-                temp = set[i];
-                set[i] = set[index];
-                set[index] = temp;
+                swap(set, i, index);
             }
-            return set;
         }
         static void printSet(int[] set)
         {
@@ -65,6 +65,7 @@ namespace NEA___Main_Code
             }
             Console.WriteLine();
         }
+       
         static int[] intListToArray(List<int> set) // converts an integer list to an integer array
         {
             int[] array = new int[set.Count];
@@ -83,17 +84,44 @@ namespace NEA___Main_Code
             }
             return list;
         }
-
-        static int[] partitionArray(int[] set, int startIndex, int endIndex) // returns the section of an array between the startIndex and endIndex
+        static int[] stitch(int[] firstArray, int middleValue, int[] secondArray) //stitches together two arrays and a value in the order inputted
         {
-            int[] setSection = new int[endIndex - startIndex + 1];
-            for(int i = 0; i < setSection.Length; i++)
+            int[] newArray = new int[firstArray.Length + 1 + secondArray.Length];
+            for(int i = 0; i < firstArray.Length; i++)
             {
-                setSection[i] = set[i + startIndex];
+                newArray[i] = firstArray[i];
             }
-            return setSection;
+            newArray[firstArray.Length] = middleValue;
+
+            int x = 0;
+            for(int i = firstArray.Length + 1;  i < newArray.Length; i++)
+            {
+                newArray[i] = secondArray[x];
+                x++;
+            }
+            return newArray;
         }
-        static int[] doCountingSort(int[] set, int range) //carries out a full counting sort with a given integer array and returns sorted array
+        static int[] stitch(int[] array, int value) //stitches together an array and a value in the order inputted
+        {
+            int[] newArray = new int[array.Length + 1];
+            for(int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+            newArray[newArray.Length-1] = value;
+            return newArray;
+        }
+        static int[] stitch( int value, int[] array) //stitches together an array and a value in the order inputted
+        {
+            int[] newArray = new int[array.Length + 1];
+            newArray[0] = value;
+            for (int i = 1; i < newArray.Length; i++)
+            {
+                newArray[i] = array[i-1];
+            }
+            return newArray;
+        }
+        static void doCountingSort(int[] set, int range) //carries out a full counting sort with a given integer array. type is void since inputting an array takes the reference
         {
 
             int[] occurences = new int[range + 1];
@@ -113,18 +141,14 @@ namespace NEA___Main_Code
                     sorted.Add(i);
                 }
             }
-            return intListToArray(sorted);
         }
-        static int[] doBubbleSort(int[] set, int range) // carries out a bubble sort with a given integer array and returns sorted array
-        {
-            while (BubblePass(ref set)) ; //will run passes of bubble sort until false is returned signalling a swap was not neccessary
-            return set;
+        static void doBubbleSort(int[] set, int range) // carries out a bubble sort with a given integer array
+        { 
+            while (BubblePass(ref set)) ; //will run passes of bubble sort until false is returned signalling a swap was not necessary
         }
         static int[] doMergeSort(int[] set) // recursively carries out a Merge sort
         {
-            int[] left;
-            int[] right;
-            int[] result = new int[set.Length];
+            int[] left, right;
             if (set.Length <= 1) //base case for when the set has been split into single elements
                 return set;
 
@@ -140,6 +164,7 @@ namespace NEA___Main_Code
             {
                 left[i] = set[i];
             }
+
             int x = 0;
 
             for (int i = midPoint; i < set.Length; i++)
@@ -150,8 +175,7 @@ namespace NEA___Main_Code
 
             left = doMergeSort(left);
             right = doMergeSort(right);
-            result = merge(left, right);
-            return result;
+            return merge(left, right);
         }
         static int[] merge(int[] left, int[] right) 
         {
@@ -191,6 +215,43 @@ namespace NEA___Main_Code
                 }
             }
             return result;
+        }
+        static arrayPair pivot(int[] set) //returns the a set of arrays for the numbers to the left and right of the pivot (first number in array)
+        {
+            int pivotValue = set[0]; 
+            List<int> left = new List<int>(); //list is used here for the ability to add as the program progresses, useful for adding lower numbers then pivot then higher numbers
+            List<int> right = new List<int>();
+
+            arrayPair result = new arrayPair();
+
+            int pivotTwinsInserted = 1; int pivotTwinsPresent = 0; //the term pivot twins is used here to denote any value equal to the pivot so as to keep track of numbers the same value as the pivot
+
+            for(int i = 1; i < set.Length; i++)
+            {
+                if(set[i] == pivotValue) pivotTwinsPresent++;
+            }
+            for(int i  = 1; i < set.Length; i++)
+            {
+                if(set[i] < pivotValue) left.Add(set[i]);
+                if(set[i] == pivotValue && pivotTwinsInserted < pivotTwinsPresent) left.Add(set[i]);
+            }
+            for(int i = 1; i < set.Length; i++)
+            {
+                if (set[i] > pivotValue) right.Add(set[i]); 
+            }
+            result.left = intListToArray(left); result.right = intListToArray(right);
+            printSet(stitch(result.left, pivotValue, result.right));
+            return result;
+        }
+        static int[] doQuickSort(int[] set)
+        {
+            if(set.Length <= 1) return set; //base case for empty array
+            int pivotValue = set[0];
+            
+            arrayPair leftAndRight = pivot(set);
+            if(leftAndRight.left.Length == 0) return doQuickSort(stitch(pivotValue,leftAndRight.right));
+            if (leftAndRight.right.Length == 0) return doQuickSort(stitch(leftAndRight.left,pivotValue));
+            return doQuickSort(stitch(doQuickSort(leftAndRight.left),pivotValue,doQuickSort(leftAndRight.right)));
         }
         static void StoreResult(string SortName, int range, int[] set, float timeTaken, long memoryUsed)
         {
@@ -236,7 +297,7 @@ namespace NEA___Main_Code
         {
             int[] setty = makeSet(true, false, 10, 10);
             printSet(setty);
-            setty = doMergeSort(setty);
+            setty = doQuickSort(setty);
             printSet(setty);
             Console.ReadKey();
         }
---
+++ NEA/testBed/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testBed
+{
+    internal class Program
+    {
+        static void printSet(int[] set)
+        {
+            for (int i = 0; i < set.Length; i++)
+            {
+                Console.Write(set[i] + "|");
+            }
+            Console.WriteLine();
+        }
+        static void changeSet(int[] set)
+        {
+            set[0] = 3; set[1] = 4; set[2] = 5;
+        }
+        static int[] stitch(int[] firstArray, int middleValue, int[] secondArray) //stitches together two arrays and a value in the order inputted
+        {
+            int[] newArray = new int[firstArray.Length + 1 + secondArray.Length];
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                newArray[i] = firstArray[i];
+            }
+            newArray[firstArray.Length] = middleValue;
+
+            int x = 0;
+            for (int i = firstArray.Length + 1; i < newArray.Length; i++)
+            {
+                newArray[i] = secondArray[x];
+                x++;
+            }
+            return newArray;
+        }
+        static void Main(string[] args)
+        {
+            int[] ints1 = { 1, 2, 3 };
+            printSet(ints1);
+            Console.ReadKey();
+            Console.WriteLine("4");
+            Console.ReadKey();
+            int[] ints2 = { 5, 6, 7 };
+            printSet(ints2);
+            Console.ReadKey();
+            printSet(stitch(ints1,4,ints2));
+            Console.ReadKey();
+        }
+    }
+}
