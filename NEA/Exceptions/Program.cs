28e9a199f304830ed6e73eebf462d02b9b5618e7
jagos <23JR3660@barton.ac.uk>
2024-08-21 19:51:54 +0100

    created some dumby subroutines

---
+++ NEA/Exceptions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public class improperPlaceValueException : Exception
+        {
+            public improperPlaceValueException(int placeValue)
+            {
+                
+            }
+            public improperPlaceValueException() : base("Place values must be some multiple of 10.")
+            {
+
+            }
+        }
+    }
+}
